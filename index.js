3b1180b89e183184dc3ad7800dab1f59f7cd5322
Shaun Tung <shaun.tung@gmail.com>
2018-01-19 08:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
-  })
-})
+
 
 function logShout(string) {
   if(string.toUpperCase === string)
@@ -22,6 +18,21 @@ function logWhisper(string) {
   if (string === lowercase)
   console.log()
 }
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
+
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
