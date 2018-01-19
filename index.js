22101957fbf42caaa88dd3b0bbc40297f157feec
Shaun Tung <shaun.tung@gmail.com>
2018-01-19 09:02:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ it('calls console.log() its one argument in all caps', function() {
 //
 
 function logShout(string) {
-  spy = string.toUpperCase
+  var spy = string.toUpperCase
   console.log(spy) 
 }
 
